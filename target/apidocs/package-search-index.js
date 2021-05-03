ea6822804e64b4747f72fe1ddc4ea0018a7c06f9
Courthouse <jsosa690@gmail.com>
2021-05-03 17:17:05 -0500

    removing unused html

--- target/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.sproutsocial.homework"},{"l":"com.sproutsocial.homework.api"},{"l":"com.sproutsocial.homework.client"},{"l":"com.sproutsocial.homework.dao"},{"l":"com.sproutsocial.homework.domain"},{"l":"com.sproutsocial.homework.mapper"},{"l":"com.sproutsocial.homework.resources"}]
